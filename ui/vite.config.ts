a212be4cf8e6142a7e65faf40d10c989e6f2a458
SparkElf <374656045@qq.com>
2023-11-10 01:00:43 +0800

    更新端口

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.HISTOIRE ? 6007 : 3000,
+  },
   build:{
     sourcemap:true
   },
